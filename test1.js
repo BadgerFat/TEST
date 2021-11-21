6474c134b4e300372344c20ce19d09d0de6c1916
BadgerFat <94040711+BadgerFat@users.noreply.github.com>
2021-11-21 19:37:58 +0700

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,27 @@
+function getStatistics(marks) {
+	var avgMark = 0;
+	var highestMark = 0;
+	var lowestMark = 5;
+	var studentnamehighestMark;
+	var studentnamelowestMark;
+	
+	for(let student of marks){
+		avgMark = avgMark + student.avgMark / marks.length;
+		if(student.avgMark > highestMark){
+			highestMark = student.avgMark;
+			studentnamehighestMark = student.name;
+		}
+		if(student.avgMark < lowestMark){
+			lowestMark = student.avgMark;
+			studentnamelowestMark = student.name;
+	}}
+	return {avgMark:avgMark, highestMark:studentnamehighestMark, lowestMark:studentnamelowestMark};		
+} 
+const testMarks = [{
+name: 'Vasya',
+avgMark: 3.75
+}, {
+name: 'Lena',
+avgMark: 4.89
+}]
+console.log(getStatistics(testMarks)); 
